8397eb1dde266ae0a9c9e890e91ac7966c7f7a03
Nikita Rehemae <nikita.rehemae@gmail.com>
2021-05-22 18:16:00 +0300

    Update index.js and index.html

--- index.js
+++ index.js
@@ -1,49 +1,31 @@
 class KY029 {
-    constructor(ctx, X = 0, Y = 0, GND = false, RED = 0, GREEN = 0) {
-        this._R = RED;
-        this._G = GREEN;
-        this._gnd = GND;
-
-        this._ctx = ctx;
-        this._x = X;
-        this._y = Y;
-        this._c = this.setColor();
+    constructor(pin1 = false, pin2 = false, pin3 = false) {
+        this.gnd = pin1
+        this.red = pin2
+        this.green = pin3
+        this.color = this.setColor()
     }
 
     setColor() {
-        this._c = "#" + 
-        (((Number(this._gnd) * 255 * this._R) . toString(16) == "0") ? "00" : ((Number(this._gnd) * 255 * this._R) . toString(16))) + 
-        (((Number(this._gnd) * 255 * this._G) . toString(16) == "0") ? "00" : ((Number(this._gnd) * 255 * this._G) . toString(16))) + "00";
+        return this.gnd ? `#${ this.red == false ? "00" : "FF" }${ this.green == false ? "00" : "FF" }${ "00" }` : "#000000"
     }
 
-    changeState(GND = false, RED = 0, GREEN = 0) {
-        this._R = RED;
-        this._G = GREEN;
-        this._gnd = GND;
-        this.setColor();
-    }
+    draw(ctx, X, Y) {
+        ctx.fillStyle = "#505050"
+        ctx.fillRect(X + 15, Y + 13, 150, 75)
+        ctx.fillStyle = "#909090"
+        ctx.fillRect(X, Y + 25, 40, 10)
+        ctx.fillRect(X, Y + 45, 40, 10)
+        ctx.fillRect(X, Y + 65, 40, 10)
+        ctx.fillStyle = this.color
+        ctx.fillRect(X + 164, Y + 22, 5, 55)
+        ctx.fillRect(X + 164, Y + 32, 40, 35)
 
-    draw() {
-        console.log(this._c);
-
-        this._ctx.fillStyle = "#505050";
-        this._ctx.fillRect(this._x + 15, this._y + 13, 150, 75);
-
-        this._ctx.font = "20px Arial";
-        this._ctx.fillStyle = "white";
-        this._ctx.fillText("KY-029", this._x + 80, this._y + 55);
-
-        this._ctx.font = "10px Arial";
-        this._ctx.fillText("-", this._x + 43, this._y + 33);
-        this._ctx.fillText("S", this._x + 43, this._y + 73);
-
-        this._ctx.fillStyle = "#909090";
-        this._ctx.fillRect(this._x, this._y + 25, 40, 10);
-        this._ctx.fillRect(this._x, this._y + 45, 40, 10);
-        this._ctx.fillRect(this._x, this._y + 65, 40, 10);
-
-        this._ctx.fillStyle = this._c;
-        this._ctx.fillRect(this._x + 164, this._y + 22, 5, 55);
-        this._ctx.fillRect(this._x + 164, this._y + 32, 40, 35);
+        ctx.font = "20px Arial"
+        ctx.fillStyle = "white"
+        ctx.fillText("KY-029", X + 80, Y + 55)
+        ctx.font = "10px Arial"
+        ctx.fillText("-", X + 43, Y + 33)
+        ctx.fillText("S", X + 43, Y + 73)
     }
 }
